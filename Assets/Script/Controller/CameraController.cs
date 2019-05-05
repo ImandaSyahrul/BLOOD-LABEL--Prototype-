34725d31a4189d25a2f3c19990d0aa64b2b11e98
ImandaSyahrul <ImandaSyahrul@github.com>
2019-05-06 05:41:08 +0700

    Add PauseMenu Controller Script
    The UI it self is not even implemented but making the script first is easier

---
+++ Assets/Script/Controller/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject followTarget; //what sprite that camera want to follow
+	private Vector3 targetPos; //target position(vector)
+	public float moveSpeed; //Camera's speed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, -10f);
+		transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Script/Controller/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float moveSpeed;
+	private Animator anim;
+	private bool PlayerMoving;
+	private Vector2 lastMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+		//Get the player animator
+		anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		PlayerMoving = false; 
+		//Move the player character
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+		{
+			transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+			PlayerMoving = true;
+			lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+		}
+		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+		{
+			transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+			PlayerMoving = true;
+			lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+		}
+
+
+		anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+		anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+		anim.SetBool("Moving", PlayerMoving);
+		anim.SetFloat("LastMoveX", lastMove.x);
+		anim.SetFloat("LastMoveY", lastMove.y);
+	}
+}
