8fbbccfc7cfe863621d65b65c38ce350136c8761
ImandaSyahrul <ImandaSyahrul@github.com>
2019-05-08 18:51:40 +0700

    Add menu

--- Assets/Script/Controller/MenuController.cs
+++ Assets/Script/Controller/MenuController.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
     // Start is called before the first frame update
     void Start()
     {
