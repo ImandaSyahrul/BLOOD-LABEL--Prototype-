b53e8c0d999058cc76e9d5a3970a2bc3c667d8e3
ImandaSyahrul <ImandaSyahrul@github.com>
2019-05-10 23:47:51 +0700

    Add OptionPanel and moved the mainmenu script into script folder
    Panel has only back button for now.

---
+++ Assets/Script/Controller/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public GameObject option;
+	public GameObject fade;
+	public Animator fadeAnim;
+
+	//public Animator anim;
+
+	private void Start()
+	{
+		fade = GameObject.Find("FadePanel");
+		option = GameObject.Find("OptionPanel");
+		option.SetActive(false);
+		fadeAnim=fade.GetComponent<Animator>();
+	}
+
+	public void PlayButton()
+    {
+		//fade.SetActive(true);
+		//fadeAnim.Play("FadeIn");
+		//System.Threading.Thread.Sleep(3000);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //diisi "nama level", (build index/ nomor) 1, SceneManager.GetActiveScene().buildIndex + 1
+    }
+
+    public void LoadButton()
+    {
+
+    }
+
+    public void OptionButton()
+    {
+		option.SetActive(true);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+
+	
+}
---
+++ Assets/Script/Controller/OptionMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionMenu : MonoBehaviour
+{
+	GameObject panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+		panel = GameObject.Find("OptionPanel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void BackButton()
+	{
+		panel.SetActive(false);
+	}
+}
