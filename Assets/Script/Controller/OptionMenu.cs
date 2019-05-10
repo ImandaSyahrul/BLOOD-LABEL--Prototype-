a2973ae42ca2e4718b83819778dc7f54a8e69a58
ImandaSyahrul <ImandaSyahrul@github.com>
2019-05-11 00:01:33 +0700

    Changed find object to "this"
    Because the script is already attached to the panel it self so instead of using Find(), its better to use this.gameObject to make it more efficient.

--- Assets/Script/Controller/OptionMenu.cs
+++ Assets/Script/Controller/OptionMenu.cs
@@ -8,7 +8,7 @@ public class OptionMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		panel = GameObject.Find("OptionPanel");
+		panel = this.gameObject;
     }
 
     // Update is called once per frame
