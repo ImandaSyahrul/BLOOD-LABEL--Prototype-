445b3b243f6119c111542eb812283e69f85e3759
HamParasUt <hamzah.prasetio.tif417@polban.ac.id>
2019-05-14 22:05:59 +0700

    penambahan script interact
    untuk interaksi, dibuat sprite "test" pada scene "Prolog" yang apabila ditekan "spasi" ketika dekat akan memunculkan log "you're interacting!" diikuti dengan nomor interaksi sebagai pembeda tiap interaksi.

---
+++ Assets/Script/Controller/Interact.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Interact : MonoBehaviour
+{
+    private int i = 0;
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.name == "Player")
+		{
+			if (Input.GetKeyDown(KeyCode.Space)) //tombol yang ditekan untuk interact, sekarang spasi dulu
+			{
+				Debug.Log("You're Interacting!"+i);
+                i++;
+			}
+		}
+	}
+}
