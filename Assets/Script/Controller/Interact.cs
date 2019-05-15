57f749ac3bebc5770a475095a4d3d9f6020f7d36
ImandaSyahrul <ImandaSyahrul@github.com>
2019-05-15 16:39:25 +0700

    Added NPC called Cortez and Player can interact with him

--- Assets/Script/Controller/Interact.cs
+++ Assets/Script/Controller/Interact.cs
@@ -5,15 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Interact : MonoBehaviour
 {
-    private int i = 0;
 	void OnTriggerStay2D(Collider2D other)
 	{
 		if (other.gameObject.name == "Player")
 		{
 			if (Input.GetKeyDown(KeyCode.Space)) //tombol yang ditekan untuk interact, sekarang spasi dulu
 			{
-				Debug.Log("You're Interacting!"+i);
-                i++;
+				Debug.Log("You're Interacting!");
+				Fungus.Flowchart.BroadcastFungusMessage("hello world first");
 			}
 		}
 	}
