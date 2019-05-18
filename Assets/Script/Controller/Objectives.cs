17afc4f98fd2bee1737818837013532a9a0dd5c4
ImandaSyahrul <ImandaSyahrul@github.com>
2019-05-18 23:29:33 +0700

    Add loadgame panel and more content for story

--- Assets/Script/Controller/Objectives.cs
+++ Assets/Script/Controller/Objectives.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Objectives : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField]
+	bool showObjective = false;
+	[SerializeField]
+	Texture objective;
+	[SerializeField]
+	private int collision;
+
+	void Start()
+	{
+		showObjective = false;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player" && showObjective == false && collision == 0)
+			showObjective = true;
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+			showObjective = false;
+		collision = 1;
+	}
+	void OnGUI()
+	{
+		if (showObjective == true)
+			GUI.DrawTexture(new Rect(Screen.width / 1.5f, Screen.height / 1.4f, 178, 178), objective);
+	}
+
+	void Update()
+	{
+		if (Input.GetButton("ShowObj") && collision == 1)
+		{
+			showObjective = true;
+		}
+		if (Input.GetButtonUp("ShowObj") && collision == 1)
+		{
+			showObjective = false;
+		}
+	}
+
 }
