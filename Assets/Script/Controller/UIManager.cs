e5a1bb4c42344efe63fd98db5e6a43ecd4643de9
ImandaSyahrul <ImandaSyahrul@github.com>
2019-05-20 02:01:21 +0700

    Merged from branch Alfia
    thanks, mantap jir zombinya juga ikut ngefreeze loh

---
+++ Assets/Script/Controller/UIManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+	public GameObject panelHUD;
+	public GameObject pausePanel;
+	public static bool gameIsPaused;
+	// Start is called before the first frame update
+	void Start()
+    {
+		pausePanel.SetActive(false);
+		panelHUD.SetActive(true);
+		gameIsPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (gameIsPaused)
+			{
+				Resume();
+			}
+			else
+			{
+				Pause();
+			}
+		}
+	}
+
+	public void Pause()
+	{
+		pausePanel.SetActive(true);
+		Time.timeScale = 0f;
+		gameIsPaused = true;
+		panelHUD.SetActive(false);
+	}
+
+	public void Resume()
+	{
+		pausePanel.SetActive(false);
+		Time.timeScale = 1f;
+		gameIsPaused = false;
+		panelHUD.SetActive(true);
+	}
+}
