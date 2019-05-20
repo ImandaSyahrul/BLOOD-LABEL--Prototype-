a8036c7c8f2e97fba1eb8c84ef995153141a4dd6
alifiasyalsa <alifiasyalsabila96@gmail.com>
2019-05-20 13:34:54 +0700

    " Box Colider in Map & Stamina Bar"
    Add  Box Colider in Dungeon Scene
    Create Stamina Bar
    - Script sliderColor
    - Modified PlayerController Script

--- Assets/Script/Controller/PlayerController.cs
+++ Assets/Script/Controller/PlayerController.cs
@@ -1,48 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-	public float moveSpeed;
-	private Animator anim;
-	private Rigidbody2D myRigidbody;
+    public float moveSpeed;
+    private Animator anim;
+    private Rigidbody2D myRigidbody;
 
-	private bool PlayerMoving;
-	private Vector2 lastMove;
+    private bool PlayerMoving;
+    private Vector2 lastMove;
+
+    //stamina bar
+    public float maxStamina;
+    public float staminaLoss = 5;
+    public Slider staminaBar;
+    public Text staminaStatus;
+
     // Start is called before the first frame update
     void Start()
     {
-		//Get the player animator
-		anim = GetComponent<Animator>();
-		myRigidbody = GetComponent<Rigidbody2D>();
+        //Get the player animator
+        anim = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
 
-		//DontDestroyOnLoad(transform.gameObject);
+        //DontDestroyOnLoad(transform.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-		PlayerMoving = false; 
-		//Move the player character
-        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
-		{
-			transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-			PlayerMoving = true;
-			lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
-		}
-		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-		{
-			transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-			PlayerMoving = true;
-			lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
-		}
+        PlayerMoving = false;
 
+        
 
-		anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
-		anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
-		anim.SetBool("Moving", PlayerMoving);
-		anim.SetFloat("LastMoveX", lastMove.x);
-		anim.SetFloat("LastMoveY", lastMove.y);
-	}
+        //Move the player character
+        if ((Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) && (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.LeftShift)))
+        {
+            maxStamina -= staminaLoss;
+            staminaBar.value = maxStamina;
+            staminaStatus.GetComponent<UnityEngine.UI.Text>().text = maxStamina.ToString();
+           
+            moveSpeed = 200;
+
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            PlayerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+
+        if ((Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) && (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.LeftShift)))
+        {
+            maxStamina -= staminaLoss;
+            staminaBar.value = maxStamina;
+            staminaStatus.GetComponent<UnityEngine.UI.Text>().text = maxStamina.ToString();
+           
+            moveSpeed = 200;
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            PlayerMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            moveSpeed = 5;
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            PlayerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+
+            moveSpeed = 5;
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            PlayerMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+        
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("Moving", PlayerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+    }
+
+    public void updateStamina()
+    {
+        maxStamina -= staminaLoss * Time.deltaTime;
+        staminaBar.value = maxStamina;
+    }
 }
---
+++ Assets/Script/sliderColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sliderColor : MonoBehaviour
+{
+     private Slider slider;
+ 
+     public int MaxHealth = 100;
+     public Image Fill;  // assign in the editor the "Fill"
+     public Color MaxHealthColor = Color.green;
+     public Color MinHealthColor = Color.red;
+ 
+     private void Start() {
+         slider = gameObject.GetComponent<Slider>();
+         slider.value = MaxHealth;        // start with full health
+     }
+
+     private void Update()
+     {
+         Fill.color = Color.Lerp(MinHealthColor, MaxHealthColor, (float)slider.value / MaxHealth);
+     }
+ }
